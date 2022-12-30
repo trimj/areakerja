de8b232bd1e8aec905f8f305cef6aadcba8dccf2
Maulana Alirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2022-12-30 10:05:42 +0700

    Menambahkan seeder dan merapikan kode

--- database/migrations/2022_12_24_170044_create_products_table.php
+++ database/migrations/2022_12_24_170044_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('keterangan');
+            $table->string('description');
             $table->integer('price');
             $table->integer('promo');
             $table->tinyInteger('promo_status');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(MitraSeeder::class);
         $this->call(JobVacancySeeder::class);
         $this->call(PriceSeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
