fba2782195c0fe4b1fb9c21a0bb5096a61074ffa
Maulana Alirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2023-01-10 11:59:08 +0700

    Update dashboard finance

--- app/Http/Controllers/FInance/DashboardFinanceController.php
+++ app/Http/Controllers/FInance/DashboardFinanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Finance;
 use App\Http\Controllers\Controller;
 use App\Models\Invoice;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class DashboardFinanceController extends Controller
@@ -12,14 +13,28 @@ class DashboardFinanceController extends Controller
     public function index()
     {
         $riwayat = Invoice::all();
-        // $saldo = [];
         $saldo['jumlah'] = 0;
         $saldo['mean'] = 0;
         if (count($riwayat) > 0) {
             $saldo['jumlah'] = $riwayat->sum('amount');
             $saldo['mean'] = $saldo['jumlah']/$riwayat->count();
         }
-        return view('finance.dashboard', compact(['riwayat', 'saldo']));
+        $data = Invoice::whereYear('created_at', now()->year -1)->get()->groupBy(function($data){
+            return Carbon::parse($data->created_at)->format('M');
+        });
+        foreach ($data as $key => $value) {
+            $amountTemp[$key] = $value->sum('amount');
+        }
+        $amount['amount']['months'] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        foreach ($amount['amount']['months'] as $key => $value) {
+            if (!isset($amountTemp[$value])) {
+                $amount['amount']['values'][] = 0;
+            } else {
+                $amount['amount']['values'][] = $amountTemp[$value];
+            }
+        }
+        $amount['max']=max($amount['amount']['values']);
+        return view('finance.dashboard', compact(['riwayat', 'saldo', 'amount']));
     }
 
     public function show($id)
@@ -28,10 +43,5 @@ class DashboardFinanceController extends Controller
         return view('finance.dashboard', compact(
             ['riwayat']
         ));
-    }
-
-    public function ajaxGetLastYearInvoice(Request $request)
-    {
-        return response()->json(Invoice::all());
     }
 }
